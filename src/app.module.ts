62fe72ee4b54291576900b1ea2afa270b8ca703f
GHAOUARI KOUSAI <91798061+koussayinsat12@users.noreply.github.com>
2025-11-26 03:59:14 -0500

    Clean up entity imports in app.module.ts
    Removed unused entity imports from app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,15 +10,6 @@ import { RecommandedCertificationsModule } from './recommanded-certifications/re
 import { RecommandedCoursesModule } from './recommanded-courses/recommanded-courses.module';
 import { TestQuizModule } from './test-quiz/test-quiz.module';
 import { QuestionsModule } from './questions/questions.module';
-import { User } from './users/entities/user.entity';
-import { Roadmap } from './roadmaps/entities/roadmap.entity';
-import { Progress } from './progress/entities/progress.entity';
-import { Validation } from './validations/entities/validation.entity';
-import { Milestone } from './milestone/entities/milestone.entity';
-import { RecommandedCertification } from './recommanded-certifications/entities/recommanded-certification.entity';
-import { RecommandedCourse } from './recommanded-courses/entities/recommanded-course.entity';
-import { TestQuiz } from './test-quiz/entities/test-quiz.entity';
-import { Question } from './questions/entities/question.entity';
 import { CommonModule } from './common/common.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
