1192c04e0ab6f6ed4346a5790a6ba5a6a7cae43b
GHAOUARI KOUSAI <91798061+koussayinsat12@users.noreply.github.com>
2025-11-26 04:07:44 -0500

    improve code readability with comments

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -3,17 +3,24 @@ import ErrorMessages from "../../utils/error.messages";
 import { UserRoleEnum } from "../../enums/user-role.enum";
 
 export class CreateUserDto {
+    // username field
     @IsNotEmpty({message:ErrorMessages.usernameRequired})
     @MinLength(4,{message:ErrorMessages.invalidLengthUsername})
     username: string;
+    
+    // email field
     @IsNotEmpty({message:ErrorMessages.emailRequired})
     @IsEmail({}, { message:ErrorMessages.invalidEmail})
     email: string;
+    
+    // password field
     @IsNotEmpty({ message: ErrorMessages.passwordRequired })
     @MinLength(6, { message:ErrorMessages.invalidLengthPassword})
     @Matches(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{6,}$/, {
       message:ErrorMessages.invalidPassword,
     })
     password: string;
+
+    // role field
     role?:UserRoleEnum;
 }
