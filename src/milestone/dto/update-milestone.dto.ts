9e9baba95abad96cf1db35ca03bd3f1c49862720
GHAOUARI KOUSAI <91798061+koussayinsat12@users.noreply.github.com>
2025-11-26 05:15:48 -0500

    Format update for UpdateMilestoneDto properties

--- src/milestone/dto/update-milestone.dto.ts
+++ src/milestone/dto/update-milestone.dto.ts
@@ -1,13 +1,15 @@
 import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class UpdateMilestoneDto {
-    @IsOptional()
-    @IsString()
-    roadmapId?:string
-    @IsOptional()
-    @IsString()
-    description?:string
-    @IsOptional()
-    @IsNumber()
-    orderNumber?:number
+  @IsOptional()
+  @IsString()
+  roadmapId?: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsNumber()
+  orderNumber?: number;
 }
